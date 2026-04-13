2f157bf4445be8a6ca46b8c6876da02afe469f41
ghostdev <ghostdev137@users.noreply.github.com>
2026-04-13 15:40:26 -0400

    apa: safety — whitelist ParkAid_Data with angle limits

--- opendbc/car/ford/interface.py
+++ opendbc/car/ford/interface.py
@@ -59,6 +59,9 @@ class CarInterface(CarInterfaceBase):
     if ret.flags & FordFlags.CANFD:
       ret.safetyConfigs[-1].safetyParam |= FordSafetyFlags.CANFD.value
 
+    if ret.flags & FordFlags.APA:
+      ret.safetyConfigs[-1].safetyParam |= FordSafetyFlags.APA.value
+
       # TRON (SecOC) platforms are not supported
       # LateralMotionControl2, ACCDATA are 16 bytes on these platforms
       if len(fingerprint[CAN.camera]):
--- opendbc/car/ford/values.py
+++ opendbc/car/ford/values.py
@@ -54,6 +54,7 @@ class CarControllerParams:
 class FordSafetyFlags(IntFlag):
   LONG_CONTROL = 1
   CANFD = 2
+  APA = 4
 
 
 class FordFlags(IntFlag):
